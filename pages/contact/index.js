2217f2b9b6ff5f8635396ba9f911845ded3ad310
madhuri390 <62072536+madhuri390@users.noreply.github.com>
2025-10-07 20:58:01 +0530

    Removing unecessary files

--- pages/contact/index.js
+++
@@ -1,9 +0,0 @@
-// import ContactUs from "../../components/ContactUs";
-
-// export default function Contact() {
-//   return (
-//     <div className="min-h-screen bg-gray-900">
-//       <ContactUs />
-//     </div>
-//   );
-// }
--- pages/solutions/index.js
+++
@@ -1,9 +0,0 @@
-// import Solution from "../../components/Solution";
-
-// export default function SOlutionsPage() {
-//   return (
-//     <div className="min-h-screen bg-gray-900">
-//       <Solution />
-//     </div>
-//   );
-// }
